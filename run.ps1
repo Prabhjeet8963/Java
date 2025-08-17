63bafd8080ed9c7d7556b163e065d573f5294aa8
PrabhjeetSingh <prabhjeet8963@gmail.com>
2025-08-18 01:13:46 +0530

    Use run.ps1 to run any code in src folder

--- run.ps1
+++ run.ps1
@@ -1,8 +1,8 @@
-# Set directories
+
 $src = "src"
 $bin = "bin"
 
-# Ask user to choose a folder
+
 Write-Host "Available folders in src/:"
 $folders = Get-ChildItem -Path $src -Directory
 $folders | ForEach-Object { Write-Host "$($_.Name)" }
@@ -12,14 +12,11 @@ $chosen = Read-Host "`nEnter the folder name you want to compile and run"
 # Get all .java files in the chosen subdirectory
 $sourceFiles = Get-ChildItem "$src\$chosen\*.java" | ForEach-Object { $_.FullName }
 
-# Compile
 javac -d $bin $sourceFiles
 
-# Build class name (assuming Main.java has public class Main)
 $classPath = $bin
 $className = "$chosen.Main"
 
-# Run
 if ($?) {
     java -cp $classPath $className
 } else {
